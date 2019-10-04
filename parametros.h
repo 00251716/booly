dbf03eceee7f07e0f0b848071f569836a59f6902
Karla Cruz <37200781+00010216@users.noreply.github.com>
2019-10-04 14:48:12 -0600

    Parámetros de analizador lexicógrafo con valores por defecto

---
+++ parametros.h
@@ -0,0 +1,10 @@
+//parametros del analizador lexicografico
+
+#ifndef GUARD_parametros_h
+#define GUARD_parametros_h
+
+#define MAXLINEA 1000  //tamaño max de una linea del programa                   
+#define MAXDIGIT    5  //max numero de digitos en los enteros             
+#define MAXID      30  //max longitud de los identificadores 
+
+#endif

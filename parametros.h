1cefd0da077598bfab094773d0ac0bc8ad777d7d
ilianairaheta <00013014@uca.edu.sv>
2019-10-05 15:57:48 -0600

    v1

--- parametros.h
+++ parametros.h
@@ -3,8 +3,8 @@
 #ifndef GUARD_parametros_h
 #define GUARD_parametros_h
 
-#define MAXLINEA 1000  //tamaño max de una linea del programa                   
-#define MAXDIGIT    5  //max numero de digitos en los enteros             
-#define MAXID      30  //max longitud de los identificadores 
-
+#define MAXLINEA 1000  //tamaño max de una linea del programa
+#define MAXPAL     11  //numero de palabras reservadas
+#define MAXDIGIT    5  //max numero de digitos en los enteros
+#define MAXID      30  //max longitud de los identificadores
 #endif

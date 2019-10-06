3bc77fa9fcf20fc062848dd593b06df22f062eca
Kevin Velásquez <kevin.vela519@gmail.com>
2019-10-06 03:10:39 -0600

    Added simple instructions functionality to the diagram and if instruction

--- ide/js/diagram.js
+++ ide/js/diagram.js
@@ -23,7 +23,7 @@ const categories = {
 
 /**
  * Colors used by instruction
- * @enum {string}
+ * @enum {string} colors
  */
 const colors = {
 	IF: "lightpink",
@@ -60,6 +60,7 @@ function init() {
     let gojs = go.GraphObject.make;
 
     initPalette(gojs);
+    initDiagram(gojs);
 }
 
 /**
@@ -146,4 +147,249 @@ function initPalette(gojs) {
 		{ key: "while", category: categories.WHILE, text: "While" },
 		{ key: "doWhile", category: categories.DOWHILE, text: "Do While" }
     ];
+}
+
+function initDiagram(gojs) {
+    let commonNodeProperties = { locationSpot: go.Spot.Center };
+    let simpleInstructionProperties = { locationSpot: go.Spot.Center, fromSpot: go.Spot.Bottom, toSpot: go.Spot.Top }
+    let endInstructionProperties = { strokeWidth: 1, width: 20, height: 20 }
+    
+    function dropOntoLink(e, obj) {
+		let diagram = e.diagram;
+		let newnode = diagram.selection.first();
+		if (!(newnode instanceof go.Node) || newnode.linksConnected.count > 0) return;
+		let oldlink = obj.part;
+		let tonode = oldlink.toNode;
+		oldlink.toNode = newnode;
+		if (simpleInstructions.includes(newnode.category)) {
+			diagram.model.addLinkData({ from: newnode.data.key, to: tonode.data.key })
+		} else {
+			endNodeKey = "end" + newnode.data.key
+			diagram.model.addNodeData({ key: endNodeKey, category: categories.ENDIF, color: colors.IF })
+			diagram.model.addLinkData({ from: newnode.data.key, to: endNodeKey, side: "Left" })
+			diagram.model.addLinkData({ from: newnode.data.key, to: endNodeKey, side: "Right" })
+			diagram.model.addLinkData({ from: endNodeKey, to: tonode.data.key })
+		}
+    }
+    
+    // Draw links between the parent and children nodes of a node being deleted.
+    function exciseNode(node) {
+		if (node === null) return;
+		var linksOut = node.findLinksOutOf();
+		var to = null;
+		if (linksOut.count > 1) {
+		  to = findMerge(node);
+		} else if (linksOut.count === 1) {  // if only one link out of the node to be deleted
+		  to = linksOut.first().toNode;
+		}
+		if (to !== null) {
+		  // now there is only a single output node to reconnect with
+		  // for all links coming into the node to be deleted
+		  var linksIn = new go.List().addAll(node.findLinksInto()).iterator;
+		  while (linksIn.next()) {
+			var l = linksIn.value;  // reconnect all links going into deleted node
+			l.toNode = to;          // to that one destination node
+		  }
+		} else {
+		  node.diagram.removeParts(node.findLinksInto(), false);
+		}
+	}
+  
+	  // If there are multiple links going out of this node,
+	  // return the node where the links merge back into one node, if any.
+	function findMerge(node) {
+		var it = node.findLinksOutOf();
+		if (it.count <= 1) return null;
+		node.diagram.nodes.each(function(n) { n._tag = 0; });
+		var i = 1;
+		while (it.next()) {
+		  var n = walkDown(it.value.toNode, i);
+		  if (n !== null) return n;
+		  i++;
+		}
+		return null;
+	}
+  
+	  // Mark all downstream nodes, but return the first node found that was already marked
+	function walkDown(node, tag) {
+		var prev = node._tag;
+		if (prev !== 0 && prev !== tag) return node;
+		node._tag = tag;
+		if (prev === tag) return null;
+		var it = node.findNodesOutOf();
+		while (it.next()) {
+			var n = walkDown(it.value, tag);
+			if (n !== null) return n;
+		}
+		return null;
+	}
+
+    myDiagram = gojs(go.Diagram, "my-diagram",
+        {
+            initialAutoScale: go.Diagram.UniformToFill,
+			layout: gojs(go.LayeredDigraphLayout, { direction: 90, setsPortSpots: false } ),
+			// Funcion que se ejecuta mientras se borra un nodo del diagrama
+			SelectionDeleting: function(e) {
+				exciseNode(e.subject.first()) // Deletes a node and its children
+			},
+			"undoManager.isEnabled": true // Enables ctrl z and ctrl y to undo and redo, respectively
+        }
+    )
+
+    
+
+    myDiagram.nodeTemplateMap.add(categories.START,
+		gojs(go.Node, "Auto",
+			{
+				locationSpot: go.Spot.Center,
+				deletable: false,
+				movable: false,
+				fromSpot: go.Spot.Bottom,
+				toSpot: go.Spot.None
+			},
+			new go.Binding("location", "loc", go.Point.parse).makeTwoWay(go.Point.stringify),
+			gojs(go.Shape, "Ellipse",
+				{ strokeWidth: 1, width:100, height:40, fill: colors.DEFAULT }
+			),
+			gojs(go.TextBlock, fontProperties, "Start")
+		)
+	);
+
+	myDiagram.nodeTemplateMap.add(categories.END,
+		gojs(go.Node, "Auto",
+			{
+				locationSpot: go.Spot.Center,
+				deletable: false,
+				movable: false,
+				fromSpot: go.Spot.None,
+				toSpot: go.Spot.Top
+			},
+			new go.Binding("location", "loc", go.Point.parse).makeTwoWay(go.Point.stringify),
+			gojs(go.Shape, "Ellipse",
+				{ strokeWidth: 1, width:100, height:40, fill: colors.DEFAULT }
+			),
+			gojs(go.TextBlock, fontProperties , "End")
+		)
+    );
+
+    myDiagram.nodeTemplateMap.add(categories.DECLARE,
+		gojs(go.Node, "Auto", simpleInstructionProperties,
+			gojs(go.Shape, "InternalStorage",
+				{ strokeWidth: 1, width: 120, height: 40, fill: colors.DECLARE }),
+			gojs(go.TextBlock, fontProperties, new go.Binding("text").makeTwoWay())
+		)
+	);
+
+    myDiagram.nodeTemplateMap.add(categories.ASSIGNMENT,
+		gojs(go.Node, "Auto", simpleInstructionProperties,
+			gojs(go.Shape, "Rectangle", 
+				{  strokeWidth: 1, width: 120, height: 40, fill: colors.ASSIGNMENT  }),
+			gojs(go.TextBlock,  fontProperties, new go.Binding("text").makeTwoWay())
+	));
+
+	myDiagram.nodeTemplateMap.add(categories.INPUT,
+		gojs(go.Node, "Auto", simpleInstructionProperties,
+			gojs(go.Shape, "Parallelogram",
+				{ strokeWidth: 1, width: 120, height: 40, fill: colors.INPUT }),
+			gojs(go.TextBlock, fontProperties, new go.Binding("text").makeTwoWay())
+		)
+	);
+
+	myDiagram.nodeTemplateMap.add(categories.OUTPUT,
+		gojs(go.Node, "Auto", simpleInstructionProperties,
+			gojs(go.Shape, "Parallelogram",
+				{ strokeWidth: 1, width: 120, height: 40, fill: colors.OUTPUT }),
+			gojs(go.TextBlock, fontProperties, new go.Binding("text").makeTwoWay())
+		)
+    );
+    
+    myDiagram.nodeTemplateMap.add(categories.IF,
+		gojs(go.Node, "Spot",
+			gojs(go.Panel, "Auto",
+				gojs(go.Shape, "Diamond", 
+					{ strokeWidth: 1, width: 80, height: 40, fill: colors.IF }),
+				gojs(go.TextBlock, fontProperties, new go.Binding("text").makeTwoWay())
+			),
+			gojs(go.Shape, "Circle",
+				{ portId: "Left", fromSpot: go.Spot.Left, stroke: null,
+				alignment: go.Spot.Left, alignmentFocus: go.Spot.Left,
+				fill: null, width: 1, height: 1 }
+			),
+			gojs(go.Shape, "Circle",
+				{ portId: "Right", fromSpot: go.Spot.Right, stroke: null,
+				alignment: go.Spot.Right, alignmentFocus: go.Spot.Right,
+				fill: null, width: 1, height: 1 }
+			)
+		)
+	);
+	
+	myDiagram.nodeTemplateMap.add(categories.ENDIF,
+		gojs(go.Node, "Auto", commonNodeProperties, { toSpot: go.Spot.LeftRightSides, fromSpot: go.Spot.Bottom },
+			gojs(go.Shape, "Circle", endInstructionProperties, new go.Binding("fill", "color")),
+		)
+    )
+    
+    myDiagram.nodeTemplateMap.add(categories.FOR,
+		gojs(go.Node, "Auto", commonNodeProperties,
+			gojs(go.Shape, "Hexagon", loopShapeProperties),
+			gojs(go.TextBlock, fontProperties, new go.Binding("text").makeTwoWay())
+		)
+	);
+
+	myDiagram.nodeTemplateMap.add(categories.WHILE,
+		gojs(go.Node, "Auto", commonNodeProperties,
+			gojs(go.Shape, "Hexagon", loopShapeProperties),
+			gojs(go.TextBlock, fontProperties, new go.Binding("text").makeTwoWay())
+		)
+	);
+
+	myDiagram.nodeTemplateMap.add(categories.DOWHILE,
+		gojs(go.Node, "Auto", commonNodeProperties,
+			gojs(go.Shape, "Hexagon", loopShapeProperties),
+			gojs(go.TextBlock, fontProperties, new go.Binding("text").makeTwoWay())
+		)
+	);
+    
+    myDiagram.model.nodeDataArray = [
+		{key: "start", category: categories.START},
+		{key: "end", category: categories.END},
+	];
+
+	myDiagram.model.linkDataArray =  [
+		{from: "start", to: "end"}
+    ];
+    
+    // Parts dragged in from the Palette will be partly translucent
+	myDiagram.findLayer("Tool").opacity = 0.5;
+
+	myDiagram.model.linkFromPortIdProperty = "side";
+
+	// Estableciendo el formato para los links dentro del diagrama
+	myDiagram.linkTemplate = gojs(go.Link,
+		{
+			routing: go.Link.AvoidsNodes,
+			selectable: false,
+			mouseDragEnter: function(e, link) { link.isHighlighted = true; },
+            mouseDragLeave: function(e, link) { link.isHighlighted = false; },
+			mouseDrop: dropOntoLink,
+			layoutConditions: go.Part.LayoutAdded | go.Part.LayoutRemoved
+		},
+		gojs(go.Shape, { strokeWidth: 2 },
+			new go.Binding("stroke", "isHighlighted", function(h) { return h ? "chartreuse" : "rgb(63,63,63)"; }).ofObject(),
+			new go.Binding("strokeWidth", "isHighlighted", function(h) { return h ? 4 : 2; }).ofObject()), // Link shape
+		gojs(go.Shape, { toArrow: "Standard" }), // Arrow shape
+		gojs(go.Panel,  // link label for conditionals, normally not visible
+            { visible: false, name: "LABEL", segmentIndex: 1, segmentFraction: 0.5 },
+            new go.Binding("visible", "", function(link) { return link.fromNode.category === categories.IF && !!link.data.text; }).ofObject(),
+            new go.Binding("segmentOffset", "side", function(s) { return s === "Left" ? new go.Point(0, 14) : new go.Point(0, -14); }),
+          )
+	);
+
+	myDiagram.addDiagramListener("ExternalObjectsDropped", function(e) {
+        var newnode = e.diagram.selection.first();
+        if (newnode.linksConnected.count === 0) {
+          // when the selection is dropped but not hooked up to the rest of the graph, delete it
+          e.diagram.commandHandler.deleteSelection();
+        }
+	});
 }

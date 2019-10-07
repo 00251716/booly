de2781684a635af0be107c42d0f0806ef0b34274
Karla Cruz <37200781+00010216@users.noreply.github.com>
2019-10-07 12:59:45 -0600

    Deleted maxpal parameter , value not defined

--- parametros.h
+++ parametros.h
@@ -4,7 +4,6 @@
 #define GUARD_parametros_h
 
 #define MAXLINEA 1000  //tamaño max de una linea del programa
-#define MAXPAL     11  //numero de palabras reservadas
 #define MAXDIGIT    5  //max numero de digitos en los enteros
 #define MAXID      30  //max longitud de los identificadores
 #endif

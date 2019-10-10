f547ab97074bf3a5cfa400cb9e4c42421930796c
Kevin Velásquez <kevin.vela519@gmail.com>
2019-10-09 20:38:49 -0600

    Added support to convert nested loops of diagram to booly language

--- ide/js/utils.js
+++ ide/js/utils.js
@@ -99,90 +99,139 @@ function hideContextMenu(){
 
 function JSONtoCompiler(){
 	var jsontext = "";
-	var object = eval('('+myDiagram.model.toJSON()+')');
+	var object = JSON.parse(myDiagram.model.toJson());
 	var links = object.linkDataArray;
 	var nodes = object.nodeDataArray;
+	let loopKeys = [];
 
 	var nextnode = nodes.find(function(elem){
 		return elem.key == links[0].to;
 	});
-	jsontext += "begin \n";
+	jsontext += "begin\n";
 	boolyPrint(nextnode,true);
 
-	function boolyPrint(node, path){
-
-		var nextnode;
-		switch(node.category){
-			case categories.DECLARE:
-				jsontext+=node.type + " " + node.variable + (node.array ? "[]" : "")+";\n";
-				var nextLink = links.find(elem => elem.from == node.key);
-					nextnode = nodes.find(elem => elem.key == nextLink.to);
-					boolyPrint(nextnode, path);
-			break;
-
-			case categories.ASSIGNMENT:
-			jsontext+=node.variable + " = " + node.expression+";\n";
-				var nextLink = links.find(elem => elem.from == node.key);
-					nextnode = nodes.find(elem => elem.key == nextLink.to);
-					boolyPrint(nextnode, path);
-			break;
-
-			case categories.INPUT:
-			jsontext+="input " + node.variable+"\n";
-				var nextLink = links.find(elem => elem.from == node.key);
-					nextnode = nodes.find(elem => elem.key == nextLink.to);
-					boolyPrint(nextnode, path);
-			break;
-
-			case categories.OUTPUT:
-			jsontext+="output " + node.expression+"\n";
-				var nextLink = links.find(elem => elem.from == node.key);
-					nextnode = nodes.find(elem => elem.key == nextLink.to);
-					boolyPrint(nextnode, path);
-			break;
-
+	function boolyPrint(node, path, indent = 1){
+		let nextnode = getNextNode(node);
+		if (loopKeys[loopKeys.length - 1] === node.key) {
+			jsontext += getIndentation(node, --indent) + "end"+node.category+"\n";
+			loopKeys.pop();
+			boolyPrint(nextnode, path, indent);
+		} 
+		else {
+			jsontext += getIndentation(node, indent);
+			switch (node.category) {
+				case categories.DECLARE:
+					jsontext+=node.type + " " + node.variable + (node.array ? "[]" : "")+";\n";
+					boolyPrint(nextnode, path, indent);
+					break;
+				case categories.ASSIGNMENT:
+					jsontext+=node.variable + " = " + node.expression+";\n";
+					boolyPrint(nextnode, path, indent);
+					break;
+				case categories.INPUT:
+					jsontext+= categories.INPUT + node.variable+";\n";
+					boolyPrint(nextnode, path, indent);
+					break;
+	
+				case categories.OUTPUT:
+					jsontext+=categories.OUTPUT + node.expression+";\n";
+					boolyPrint(nextnode, path, indent);
+					break;
+	
+				case categories.IF:
+					jsontext+="if ("+node.condition+")"+"\n";
+					// Making a copy of the next node on the right side
+					// to avoid refind it later
+					let rightNode = nextnode;
+					//true path
+					nextnode = getNextNode(node, true);
+					boolyPrint(nextnode,false, indent + 1);
+					//false path
+					if (rightNode.category !== categories.ENDIF){
+						jsontext += getIndentation(indent) + "else "+"\n";
+					} else {
+						jsontext = jsontext.substring(0, jsontext.length - indent);
+					}
+					boolyPrint(rightNode,true, ++indent);
+					break;
+	
+				case categories.ENDIF:
+					if (path) {
+						jsontext+="endif"+"\n";
+						boolyPrint(nextnode, path, --indent);
+					}
+					break;
+	
+				case categories.FOR:
+					jsontext += categories.FOR + " (inty "+node.variable+" = "+node.startValue + "; "
+								+node.variable + " = "+ node.endValue + "; "
+								+node.variable +" = " + node.variable +" + "+ node.step + ")\n";
+					nextnode = getNextNode(node, true);
+					loopKeys.push(node.key);
+					boolyPrint(nextnode,path, ++indent);
+					break;
+	
+				case categories.WHILE:
+					jsontext += "whiley(inty "+node.condition+")\n";
+					//loop path
+					nextnode = getNextNode(node, true);
+					boolyPrint(nextnode,path, ++indent);
+					break;
+	
+				case categories.END:
+					jsontext+="end"+"\n";
+					break;
+				default: jsontext+="-"+"\n";
+			}
+		}
+	}
+	
+	function getNextNode(node, isTrueValue = false) {
+		let nextLink;
+		switch (node.category) {
 			case categories.IF:
-			jsontext+="if ("+node.condition+")"+"\n";
-				//true path
-				var leftLink = links.find(elem => elem.from == node.key && elem.fromPort == "Left");
-				nextnode = nodes.find(elem => elem.key == leftLink.to);
-				boolyPrint(nextnode,false);
-				//false path
-				var rightLink = links.find(elem => elem.from == node.key && elem.fromPort == "Right");
-				nextnode = nodes.find(elem => elem.key == rightLink.to);
-				if (nextnode.category !== categories.ENDIF){
-					jsontext+="else "+"\n";
-					boolyPrint(nextnode,true);
+				if (isTrueValue) {
+					nextLink = links.find(elem => elem.from == node.key && elem.fromPort === linkNames.IFTRUE);
 				}
-			break;
-
-			case categories.ENDIF:
-				if(path){
-					jsontext+="endif"+"\n";
-					var nextLink = links.find(elem => elem.from == node.key);
-					nextnode = nodes.find(elem => elem.key == nextLink.to);
-					boolyPrint(nextnode, path);
+				else {
+					nextLink = links.find(elem => elem.from == node.key && elem.fromPort === linkNames.IFFALSE);
 				}
-			break;
-
-			/*case categories.FOR:
-				console.log("for ("+node.variable+" = "+node.startValue + " to "+node.endValue + " step "+node.step+")");
-				//loop path
-				var loopLink = links.find(elem => elem.from == node.key && elem.side == "Right");
-				nextnode = nodes.find(elem => elem.key == loopLink.to);
-				boolyPrint(nextnode,false);
-				//false path
-				var rightLink = links.find(elem => elem.from == node.key && elem.side == "Right");
-				nextnode = nodes.find(elem => elem.key == rightLink.to);
-				if (nextnode.category !== categories.ENDIF)
-					console.log("else ");
-					boolyPrint(nextnode,true);
-			break;*/
-
+				break;
+			case categories.FOR:
+			case categories.WHILE:
+			case categories.DOWHILE:
+				if (isTrueValue) {
+					nextLink = links.find(elem => elem.from == node.key && elem.fromPort === linkNames.LOOPFROM);
+				}
+				else {
+					nextLink = links.find(elem => elem.from == node.key && elem.fromPort !== linkNames.LOOPFROM);
+				}
+				break;
 			case categories.END:
-				jsontext+="end"+"\n";
-			break;
-			default: jsontext+="-"+"\n";
+				return;
+			default:
+				nextLink = links.find(elem => elem.from == node.key);
+				break;
+		}
+		let nextnode = nodes.find(elem => elem.key == nextLink.to);
+		return nextnode;
+	}
+
+	/**
+	 * Returns the indentantion that correspondes to the especified node based
+	 * on the current level of indentation	
+	 * @param {go.Node} node current node to be indentend
+	 * @param {Integer} level 
+	 */
+	function getIndentation(node, level) {
+		let tab = "\t";
+		switch (node.category) {
+			case categories.ENDIF:
+				return tab.repeat(level - 1);
+			case categories.END:
+				return "";
+			default:
+				return tab.repeat(level);
 		}
 	}
 

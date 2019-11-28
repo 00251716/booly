2b4a581e1979b1fbf22b273061cda4bf2f623858
ilianairaheta <00013014@uca.edu.sv>
2019-11-27 20:42:42 -0600

    Rollback of class functions

--- compiler/parser/parser.py
+++ compiler/parser/parser.py
@@ -7,7 +7,6 @@ from lexer.token_kind import TokenKind as Tk
 from lexer.token import Token
 
 
-
 class Parser:
     def __init__(self, text):
         self.lexer = Lexer(text)
@@ -20,3 +19,32 @@ class Parser:
                 pass
             if token.kind == Tk.EndOfFileToken:
                 break
+
+    @property
+    def current(self):
+        return self.peek(0)
+
+    def move(self):
+        self.position += 1
+
+    def get_token_and_move(self):
+        current = self.current
+        self.move()
+        return current
+
+    def peek(self, offset):
+        index = self.position + offset
+        if index >= len(self.tokens):
+            return self.tokens[-1]
+        return self.tokens[index]
+
+    def match(self, kind):
+        return self.current.kind == kind
+
+    def match_and_get(self, *kinds):
+        last_kind = kinds[-1]
+        expected_tokens = ""
+        for kind in kinds:
+            if self.current.kind == kind:
+                return self.get_token_and_move()
+            expected_tokens += kind.name + (""  if last_kind == kind else ", ")

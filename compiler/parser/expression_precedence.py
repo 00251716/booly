43664bf1f8090480c1ccc6bc03c6b87e6dcce4cb
Kevin Velásquez <kevin.vela519@gmail.com>
2019-11-28 01:41:59 -0600

    Adding support for logical operators precedence
    Co-authored-by: Karla Cruz <00010216@users.noreply.github.com>

--- compiler/parser/expression_precedence.py
+++ compiler/parser/expression_precedence.py
@@ -3,17 +3,24 @@ from lexer.token_kind import TokenKind as Sk
 
 def get_unary_precedence(kind: Sk):
     if kind in {Sk.PlusToken, Sk.MinusToken, Sk.BangToken}:
-        return 4
+        return 7
     else:
         return 0
 
 
 def get_binary_precedence(kind: Sk):
     if kind == Sk.HatToken:
-        return 3
+        return 6
     if kind in {Sk.StarToken, Sk.SlashToken, Sk.PercentToken}:
-        return 2
+        return 5
     elif kind in {Sk.PlusToken, Sk.MinusToken}:
+        return 4
+    elif kind in {Sk.EqualsToken, Sk.NotEqualsToken, Sk.LessToken, Sk.LessEqualsToken, Sk.GreaterEqualsToken,
+                  Sk.GreaterToken}:
+        return 3
+    elif kind == Sk.AndToken:
+        return 2
+    elif kind == Sk.OrToken:
         return 1
     else:
         return 0

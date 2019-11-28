ff48dc07f53e63d3adb8b6bff21f9186fbaa6a88
Kevin Lopez <00251716@uca.edu.sv>
2019-11-27 18:51:10 -0600

    error recognition for binary expression implemented

--- compiler/ast/binary_expression.py
+++ compiler/ast/binary_expression.py
@@ -1,6 +1,7 @@
 from ast.expression import Expression
 from symbol.type import get_max_data_type
 from ast.node_kind import AstNodeKind
+from diagnostic.diagnostics import report_incompatible_types
 
 class BinaryExpression(Expression):
     def __init__(self, expr1, operator, expr2):
@@ -9,6 +10,7 @@ class BinaryExpression(Expression):
         self.right = expr2
         self.data_type = get_max_data_type(expr1.data_type, expr2.data_type)
         if self.data_type is None:
+            report_incompatible_types(expr1.data_type.name, expr2.data_type.name, operator)
         self.kind = AstNodeKind.BinaryExpression
 
     def __str__(self):
---
+++ compiler/diagnostic/diagnostics.py
@@ -0,0 +1,11 @@
+from lexer.token_kind import TokenKind
+from lexer.token import Token
+
+diagnostics = []
+
+def report(message):
+    diagnostics.append(message)
+
+def report_incompatible_types(type1, type2, operator: Token):
+    message = "ERROR: Incompatible data types <{}> and <{}> for operator <{}> on line {}".format(type1, type2, operator.lexeme, operator.line_number)
+    report(message)

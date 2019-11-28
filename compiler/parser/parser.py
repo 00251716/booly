b3d649326267611f18065166505c1e1d23cd445c
00010216 <00010216@uca.edu.sv>
2019-11-27 21:08:19 -0600

    updated parse expression
    Co-Authored-By: Kevin Velasquez <kevinv519@users.noreply.github.com>

--- compiler/parser/parser.py
+++ compiler/parser/parser.py
@@ -5,7 +5,7 @@ from ast.unary_expression import UnaryExpression
 from lexer.lexer import Lexer
 from lexer.token_kind import TokenKind as Tk
 from lexer.token import Token
-
+from parser.expression_precedence import get_binary_precedence, get_unary_precedence
 
 class Parser:
     def __init__(self, text):
@@ -48,3 +48,23 @@ class Parser:
             if self.current.kind == kind:
                 return self.get_token_and_move()
             expected_tokens += kind.name + (""  if last_kind == kind else ", ")
+
+    def parse(self):
+        return self.parse_expression()
+
+    def parse_expression(self, parent_precedence=0):
+        unary_precedence = get_unary_precedence(self.current.kind)
+        if unary_precedence != 0:
+            operator = self.get_token_and_move()
+            operand = self.parse_expression(unary_precedence)
+            left = UnaryExpression(operator, operand)
+        else:
+            left = self.factor()
+        while True:
+            precedence = get_binary_precedence(self.current.kind)
+            if precedence == 0 or precedence <= parent_precedence:
+                break
+            operator = self.get_token_and_move()
+            right = self.parse_expression(precedence)
+            left = BinaryExpression(left, operator, right)
+        return left

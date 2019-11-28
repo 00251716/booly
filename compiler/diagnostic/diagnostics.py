dcc6ce04833ff8228f0b070e4bb61012b11458b3
gammanc <00215316@uca.edu.sv>
2019-11-27 21:44:28 -0600

    added error for unexpected tokens
    Co-Authored-By: Kevin Velasquez <kevinv519@users.noreply.github.com>

--- compiler/diagnostic/diagnostics.py
+++ compiler/diagnostic/diagnostics.py
@@ -10,6 +10,14 @@ def report_bad_character(line, char):
     message = "ERROR: Bad character input:'{}' on line {}".format(char, line)
     report(message)
 
+def report_unexpected_token(current: Token, expected: Token):
+    message = "ERROR: Unexpected token <{}>, expected <{}>".format(current.kind.name, expected.kind.name)
+    report(message)
+
+def report_unexpected_tokens(current: Token, *expected):
+    message = "ERROR: Unexpected token <{}>, expected one of the following <{}>".format(current.kind.name, expected)
+    report(message)
+
 def report_incompatible_types(type1, type2, operator: Token):
     message = "ERROR: Incompatible data types <{}> and <{}> for operator <{}> on line {}".format(type1, type2, operator.lexeme, operator.line_number)
     report(message)

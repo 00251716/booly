f90f8456c177d24d90552bf63b9324ed4c26d21f
gammanc <00215316@uca.edu.sv>
2019-11-27 22:36:25 -0600

    adding new error messages for unary expression

--- compiler/diagnostic/diagnostics.py
+++ compiler/diagnostic/diagnostics.py
@@ -1,23 +1,36 @@
 from lexer.token_kind import TokenKind
 from lexer.token import Token
+from symbol.type import DataType
 
 diagnostics = []
 
+
 def report(message):
     diagnostics.append(message)
+
 
 def report_bad_character(line, char):
     message = "ERROR: Bad character input:'{}' on line {}".format(char, line)
     report(message)
 
+
 def report_unexpected_token(current: Token, expected: Token):
     message = "ERROR: Unexpected token <{}>, expected <{}>".format(current.kind.name, expected.kind.name)
     report(message)
+
 
 def report_unexpected_tokens(current: Token, *expected):
     message = "ERROR: Unexpected token <{}>, expected one of the following <{}>".format(current.kind.name, expected)
     report(message)
 
+
 def report_incompatible_types(type1, type2, operator: Token):
-    message = "ERROR: Incompatible data types <{}> and <{}> for operator <{}> on line {}".format(type1, type2, operator.lexeme, operator.line_number)
+    message = "ERROR: Incompatible data types <{}> and <{}> for operator <{}> on line {}".format(type1, type2,
+                                                                                                 operator.lexeme,
+                                                                                                 operator.line_number)
+    report(message)
+
+
+def report_undefined_unary_operator(operator_text, operand_type: DataType):
+    message = "ERROR: Unary operator <{}> is not defined for data type <{}>".format(operator_text, operand_type.name)
     report(message)

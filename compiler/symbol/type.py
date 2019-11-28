084b13a56cc6fc461d4bf8ee43b3ec119df69c81
Kevin Lopez <00251716@uca.edu.sv>
2019-11-27 19:50:20 -0600

    class to manage data types added

---
+++ compiler/symbol/type.py
@@ -0,0 +1,29 @@
+from symbol.symbol import Symbol
+
+
+class DataType(Symbol):
+    def __init__(self, name):
+        super().__init__(name)
+
+
+DataType.Bool = DataType("booly")
+DataType.Char = DataType("chary")
+DataType.Int = DataType("inty")
+DataType.Float = DataType("floaty")
+DataType.String = DataType("stringy")
+DataType.Error = DataType("?")
+
+
+def is_numeric(dt):
+    return dt in {DataType.Char, DataType.Int, DataType.Float}
+
+
+def get_max_data_type(expression1, expression2):
+    if not is_numeric(expression1) or not is_numeric(expression2):
+        return None
+    elif expression1 == DataType.Float or expression2 == DataType.Float:
+        return DataType.Float
+    elif expression1 == DataType.Int or expression2 == DataType.Int:
+        return DataType.Int
+    else:
+        return DataType.Char
